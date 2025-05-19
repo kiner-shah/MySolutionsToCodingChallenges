d7fbead0c808f47da4207364099a8bac9082c5e6
Kiner Shah <kiner.shah1@gmail.com>
2025-05-19 13:24:56 +0530

    Added calculator

---
+++ calculator-tool/README.md
@@ -0,0 +1,4 @@
+Implemented calculator tool as per the [challenge description](https://codingchallenges.fyi/challenges/challenge-calculator).
+
+Supports basic operations - add, subtract, multiply, divide, negate (unary minus)
+Supports functions: sin, cos, tan, log, exp
---
+++ calculator-tool/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+./KCalculator '2'
+./KCalculator '1-2+3'
+./KCalculator '-2+3/5*(-6/3)'
+./KCalculator 'sin(-2)*3.14*100'
+./KCalculator '4/3/-2/1*5*6*7'
+./KCalculator '-1/(2*3+(4/5))'
+./KCalculator 'sin(34)/cos(34)/tan(34)*2'
+./KCalculator '-1+2/4*-3+sin(-20.234e+203-30)+cos(20-(30/4))'
+
+./KCalculator '-1+(1+)'
+./KCalculator '2/tan()/-2'
+./KCalculator '2/*3'
