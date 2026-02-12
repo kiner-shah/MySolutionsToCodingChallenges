1f1639f3eb325a6d09713aa8432749d86cd6a8d3
Kiner Shah <kiner.shah1@gmail.com>
2026-02-12 16:29:32 +0530

    Logic simplification - use char instead of char32

--- compression-tool/HuffmanTree.hpp
+++ compression-tool/HuffmanTree.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <deque>
+#include <queue>
 #include <cstdint>
 #include <optional>
 #include <string>
@@ -11,12 +11,12 @@ namespace kcompress
 {
 struct HuffmanTreeNode
 {
-    std::optional<char32_t> m_utf32_codepoint;
+    std::optional<unsigned char> m_char;
     std::uint64_t m_value;
     HuffmanTreeNode* m_left;    // doesn't take ownership (doesn't free memory)
     HuffmanTreeNode* m_right;   // doesn't take ownership (doesn't free memory)
 
-    explicit HuffmanTreeNode(std::optional<char32_t> wc = std::nullopt, std::uint64_t value = 0, HuffmanTreeNode* left = nullptr, HuffmanTreeNode* right = nullptr);
+    explicit HuffmanTreeNode(std::optional<unsigned char> c = std::nullopt, std::uint64_t value = 0, HuffmanTreeNode* left = nullptr, HuffmanTreeNode* right = nullptr);
     ~HuffmanTreeNode();
 
     bool is_leaf_node() const;
@@ -25,21 +25,23 @@ struct HuffmanTreeNode
 
 class HuffmanTree
 {
-    using BitMapType = std::unordered_map<char32_t, std::string>;
+    using BitMapType = std::unordered_map<unsigned char, std::string>;
+    using FrequencyMapType = std::unordered_map<unsigned char, std::uint64_t>;
+    using HeapType = std::priority_queue<HuffmanTreeNode*, std::vector<HuffmanTreeNode*>, std::greater<>>;
 
     HuffmanTreeNode* m_root;
-    std::uint32_t m_tree_height;
     BitMapType m_bit_map;
+    std::uint32_t m_tree_height;
 
-    void build_tree(std::deque<HuffmanTreeNode*>& nodes);
+    void build_tree(HeapType& nodes);
     void destroy_tree(HuffmanTreeNode* root);
-    void construct_bit_map(HuffmanTreeNode* root, std::string value);
+    void construct_bit_map(HuffmanTreeNode* root, std::string& value);
     std::uint32_t get_tree_height(HuffmanTreeNode* root) const;
     HuffmanTreeNode* deserialize(const std::vector<unsigned char>& serialized_tree, std::uint64_t serialized_tree_bits,
                     std::size_t& index, std::uint64_t& total_processed_bits, unsigned char& remaining_bits, HuffmanTreeNode* root);
 public:
     HuffmanTree();
-    HuffmanTree(const std::unordered_map<char32_t, std::uint64_t>& char32_frequency_map);
+    HuffmanTree(const FrequencyMapType& frequency_map);
     ~HuffmanTree();
 
     BitMapType get_bit_map() const;
@@ -47,8 +49,8 @@ public:
     void print_tree();
 
     std::vector<unsigned char> serialize(std::uint64_t& total_bits);
-    std::vector<unsigned char> serialize_payload(const std::vector<char32_t>& codepoint_sequence, std::uint64_t& total_bits);
+    std::vector<unsigned char> serialize_payload(const std::vector<unsigned char>& char_sequence, std::uint64_t& total_bits);
     void deserialize(const std::vector<unsigned char>& serialized_tree, std::uint64_t serialized_tree_bits);
-    std::vector<char32_t> deserialize_payload(const std::vector<unsigned char>& serialized_payload, std::uint64_t serialized_payload_bits);
+    std::vector<unsigned char> deserialize_payload(const std::vector<unsigned char>& serialized_payload, std::uint64_t serialized_payload_bits);
 };
 }   // namespace kcompress
